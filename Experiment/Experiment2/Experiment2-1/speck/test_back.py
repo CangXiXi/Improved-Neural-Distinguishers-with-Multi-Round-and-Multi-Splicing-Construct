fa60b8e9d798aa094ff3609a43309fb7ffec8d22
CangXiXi <92219003+CangXiXi@users.noreply.github.com>
2022-09-19 21:39:16 +0800

    Add files via upload

---
+++ Experiment/Experiment2/Experiment2-1/speck/test_back.py
@@ -0,0 +1,12 @@
+import speck as sp
+import numpy as np
+from os import urandom
+from tensorflow.keras.models import load_model
+wdir = './'
+n = 10**6
+rounds = [5,6]
+for i in rounds:
+    model = load_model('best'+str(i)+'depth5.h5')
+    x,y= sp.make_testback_data(n, nr=i, diff=(0x0040,0))
+    scores = model.evaluate(x,y)
+    np.save(wdir + 'evaluate_scores_r'+str(i)+'.npy',scores);
